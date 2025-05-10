0ea5cec4d57917ff497920604c8cffa948225373
nbossi <bossi.angel133@gmail.com>
2025-05-10 23:53:32 +0200

    Amélioration de l'IHM console : ajout de couleurs et refonte visuelle 🔁 Création de la fonction générique get_amount() utilisée par deposit() et withdraw()

--- main.py
+++ main.py
@@ -1,98 +1,102 @@
+from colorama import init, Fore, Style
 
-def show_balance(balance):
-    print(f"Your actual balance is ${balance:.2f}.") 
+# Initialise colorama (utile sur Windows)
+init(autoreset=True)
 
-def deposit(): 
-    user_input = input("Enter an amount to be deposited (or type 'quit' to return): ").strip()
-    if user_input.lower() == "quit":
-        print("Returning to main menu.")
-        return 0
-    try:
-        amount = float(user_input) 
-        if amount <= 0:
-            print("That's not a valid amount.")
-            return 0 
-        else:
-            return amount 
-    except ValueError:
-        print("Invalid input.")    
-        return deposit()
-        
-    
-def withdraw(balance):
-    user_input = input("Enter amount to be withdrawn (or type 'quit' to return): ").strip()
-    if user_input.lower() == "quit":
-        print("Returning to main menu.")
-        return 0
-    try:
-        amount = float(user_input)
-        if amount > balance:
-            print("Insufficients funds.")
-            return 0
-        elif amount < 0:
-            print("Amount must be greater than 0.")
-            return 0
-        else:
-            return amount
-    except ValueError:
-        print("Invalid input.")
-        return withdraw(balance)
+# Visuel
+def print_header(title):
+    print(Style.BRIGHT + Fore.CYAN + f" MENU {title.upper()} ".center(40, "*"))
+
+def print_menu():
+    menu_options = {
+        "1": "Show Balance",
+        "2": "Deposit",
+        "3": "Withdraw",
+        "4": "View Transactions",
+        "5": "Exit"
+    }
+    print("\n" + Style.BRIGHT + Fore.CYAN + " MAIN MENU ".center(40, "="))
+    for key, value in menu_options.items():
+        print(Style.BRIGHT + Fore.CYAN + f"\t{key}. {value}" )
+    print(Style.BRIGHT + Fore.CYAN +"="*40)
 
 def add_delimitation(type_delimitation = "-", length = 30):
-    for i in range(length):
-        print(type_delimitation, end="")
-    print()  # pour ajouter un saut de ligne après la ligne de délimitation
+    print(Fore.CYAN + type_delimitation*length)
+    # for i in range(length):
+    #     print(type_delimitation, end="")
+    # print()  # pour ajouter un saut de ligne après la ligne de délimitation
 
+# Transactions basiques
+def show_balance(balance):
+    print(Fore.GREEN +f"Your actual balance is ${balance:.2f}.") 
+
+def get_amount(prompt, max_amount=None):
+    is_amount = True
+    while is_amount:
+        print(Fore.CYAN + prompt, end='')
+        user_input = input(Fore.MAGENTA).strip()
+        if user_input.lower() == "quit":
+            is_amount = False
+            return 0
+        try:
+            amount = float(user_input)
+            if amount < 0:
+                print(Fore.RED + "Amount must be positive")
+                continue
+            elif (max_amount or max_amount == 0) and amount > max_amount:
+                print(Fore.RED + f"Amount exceeds available ${max_amount:.2f}")
+                continue
+            return amount
+        except ValueError:
+            print(Fore.RED + "Invalid amount. Please enter a number")
+            
+def deposit(): 
+    return get_amount("Enter an amount to be deposited (or type 'quit' to return): ")
+    
+def withdraw(balance):
+    return get_amount("Enter amount to be withdrawn (or type 'quit' to return): ", balance)
     
 def main():
-    add_delimitation()
-    print(" Welcome to Your Banking App")
-    add_delimitation()
+    add_delimitation('_', 40)
+    print("\n" +Fore.CYAN + "Welcome to Your Banking App".center(40, " "))
+    add_delimitation('_', 40)
 
     balance = 0
     is_running = True 
 
     while is_running:
-        print("Banking Menu")
-        print("\t1️. Show Balance")
-        print("\t2️. Deposit")
-        print("\t3️. Withdraw")
-        print("\t4️. Exit")
-        add_delimitation()
-        choice = input("Enter your choice (1-4): ").strip()
+        print_menu()
+        print(Fore.CYAN + "Enter your choice (1-5): ", end='')
+        choice = input(Fore.MAGENTA).strip()
         
         print()  # Ligne vide pour l'aération
 
         if choice == '1': # because actually input is a string data
-            add_delimitation()
-            print("***** Section Balance *****") 
+            print_header("balance")
             show_balance(balance)
-            add_delimitation()
 
         elif choice == '2':
-            add_delimitation()
-            print("***** Section Deposit *****") 
+            print_header("DEPOSIT")
             show_balance(balance)
             balance += deposit()
             show_balance(balance)
-            add_delimitation()
         elif choice == '3':
-            add_delimitation()
-            print("***** Section Withdraw *****") 
+            print_header("WITHDRAW")
             show_balance(balance)
             balance -= withdraw(balance)
             show_balance(balance)
-            add_delimitation()
         elif choice == '4':
+            print_header("TRANSACTIONS")
+            print("TODO ... sorry")
+        elif choice == '5':
             is_running = False 
-            print("Exiting... Thank you for banking with us!")
-            add_delimitation()
+            print(Fore.MAGENTA + "Exiting... Thank you for banking with us!")
         else:
-            add_delimitation()
-            print("That is not a valid choice. Please enter 1-4.")
-            add_delimitation()
-        
-    print("Thank you! Have a nice day!")
-    
+            print(Fore.RED + "That is not a valid choice. Please enter 1-5.")
+            
+    add_delimitation('_', 40)
+    print(Fore.MAGENTA + "\nThank you! Have a nice day!")
+    add_delimitation('_', 40)
+
 if __name__ == "__main__":
     main()
